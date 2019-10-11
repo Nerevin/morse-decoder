f4d109806ac4b92ec23f3ad9e6d4e4f22bafa2a6
Nerevin <kazak.redalerd@gmail.com>
2019-10-11 22:57:31 +0400

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -36,9 +36,47 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
-    // write your solution here
+    let mas = [];
+    let j = 0;
+    let output = '';
+    for (let i = 0; i < expr.length; i += 10) {
+            mas[j] = expr.substr(i, 10);
+            j++;
+        }
+    for (let i = 0; i < mas.length; i++) {
+        let j = 0;
+        let result = '';
+        let letter = mas[i];
+        while (j < letter.length) {
+            if (letter[j] + letter[j+1] == '00') {
+                result += '';
+                j+=2;
+            }
+            else if (letter[j] + letter[j+1] == '10') {
+                result += '.';
+                j+=2;
+            }
+            else if (letter[j] + letter[j+1] == '11') {
+                result += '-';
+                j+=2;
+            }
+            else {
+                result = ' ';
+                j = 10;
+            }
+        }
+        mas[i] = result;
+    }
+    for (let i = 0; i < mas.length; i++) {
+        if (mas[i] != " ") {
+            output += MORSE_TABLE[mas[i]];
+        }
+        else {
+            output += " ";
+        }
+    }
+    return output;
 }
 
 module.exports = {
